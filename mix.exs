d2d9145b8e69fbd71050059bfbb30497215cc2a3
Danielwsx64 <danielws.x64@gmail.com>
2021-04-24 15:25:12 -0300

    Add package config

--- mix.exs
+++ mix.exs
@@ -1,17 +1,36 @@
 defmodule PluggyElixir.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+  @description "A client library for Pluggy.ai API"
+  @links %{"GitHub" => "https://github.com/brainnco/pluggy_elixir"}
+
   def project do
     [
       app: :pluggy_elixir,
-      version: "0.0.1",
-      elixir: "~> 1.11",
+      version: @version,
+      description: @description,
+      source_url: @links["GitHub"],
+      package: package(),
+      docs: docs(),
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.json": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   def application do
     [
       extra_applications: [:logger],
@@ -19,11 +38,30 @@ defmodule PluggyElixir.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Dev/Test dependencies
+      {:credo, "~> 1.5", only: [:dev, :test]},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.14", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: @links
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      extras: [
+        "README.md": [title: "Get starting"]
+      ],
+      groups_for_modules: []
     ]
   end
 end
